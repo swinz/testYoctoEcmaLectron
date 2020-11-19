30c1c3f25a0f10ca93bd15e3549319cddc0c2bdb
Fake Bob Dobbs <swinz@grtmail.com>
2020-11-18 20:09:27 -0500

    vue config

---
+++ hello-yocto/vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true,
+      builderOptions: {
+        // options placed here will be merged with default configuration and passed to electron-builder
+        files: ["**/*"],
+        extraFiles: [
+          {
+            from: "VirtualHub",
+            to: "VirtualHub",
+            filter: ["**/*"]
+          }
+        ]
+      }
+    }
+  }
+};
