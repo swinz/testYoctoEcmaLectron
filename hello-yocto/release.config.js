deb11970e4c47c5738bc6d84d2d91f107f31eff1
Stefan Winz <swinz@dividebyzero.local>
2021-02-19 21:59:19 -0500

    initial

---
+++ hello-yocto/release.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "plugins": [
+      "@semantic-release/commit-analyzer",
+      "@semantic-release/release-notes-generator",
+      "@semantic-release/changelog",
+    //   ["@semantic-release/npm", {
+    //     "tarballDir": "release"
+    //   }],
+      "@semantic-release/npm",
+    //   ["@semantic-release/github", {
+    //     "assets": "release/*.tgz"
+    //   }],
+      "@semantic-release/github",
+      "@semantic-release/git"
+    ],
+    "preset": "angular"
+  }
